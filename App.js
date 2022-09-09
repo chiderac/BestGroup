e404831901123344e3b098f08f6a726972ed1f5d
Polly <plinkoglegabriel@gmail.com>
2022-09-09 15:06:39 +0100

    commit polly

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
+    <ImageBackground 
+    source={{uri: 'https://i.pinimg.com/564x/64/31/35/643135e60d9de9543ade17d3b96fff8a.jpg'}}
+    style={styles.backgroundImage}
+  >
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Horoscope App Title</Text>
       <StatusBar style="auto" />
     </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover'
+  },
+  text: {
+    color: "white",
+    fontSize: 33,
+    fontFamily: "SFUIDisplay-Bold"
+  }
 });
