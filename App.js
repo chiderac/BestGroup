87b956e7670f908e30abba29a71330f9b0d97971
Chidera Chukwumah <38589646+chiderac@users.noreply.github.com>
2022-09-12 19:35:36 +0100

    Background and Text

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground} from 'react-native';
+
 
 export default function App() {
+  const localImage = require('./assets/WallpaperDog-996754.jpg');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ImageBackground source={localImage }style={styles.container}>
+      <Text style={styles.title}>Astrology</Text>
+      <Text style={styles.subheader}>Click on your star sign to get your daily horoscope</Text>
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
   },
+  title: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 60,
+  },
+  header: {
+      color: "black",
+      fontWeight: "bold",
+      fontSize: 20,
+  },
+  subheader: {
+      color: "white",
+      fontWeight: "bold",
+      fontSize: 16,
+  },
+  text: {
+      color: "black",
+      fontSize: 14,
+  }
+
 });
